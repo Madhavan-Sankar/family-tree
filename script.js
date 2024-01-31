9d57347da84f8eee48570b72ba34d33f73bc358c
Madhavan B <madhavansankar007@gmail.com>
2024-01-31 21:11:47 +0530

    trail version 1

--- script.js
+++ script.js
@@ -13,17 +13,3 @@ photoInput.addEventListener('change', (event) => {
     reader.readAsDataURL(file);
   }
 });
-
-
-window.addEventListener('load', () => {
-    const message = document.createElement('p');
-    const storedPhoto = localStorage.getItem('userPhoto');
-    if (storedPhoto) {
-        // Display the stored photo instead of uploading again
-        previewImage.src = storedPhoto;
-    } else {
-        alert("Please upload a photo first!");
-    }
-    document.body.appendChild(message);
-  });
-  
